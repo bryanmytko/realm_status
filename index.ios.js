105bb948271ecfa270552c1e846edd7c6e2a502a
Bryan Mytko <bryanmytko@gmail.com>
2016-09-16 16:16:11 -0400

    Test message - working iOS build

--- index.ios.js
+++ index.ios.js
@@ -17,7 +17,7 @@ class realm_status extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to Realm Status
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
